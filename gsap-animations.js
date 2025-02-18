c649bf09b6dd1740e132616cc7517764ee151cef
hub-mol <113196877+hub-mol@users.noreply.github.com>
2025-02-18 22:53:41 +0100

    edit gsap file

--- gsap-animations.js
+++ gsap-animations.js
@@ -1,23 +1,29 @@
-// import { gsap } from "gsap";
-// import { Flip } from "gsap/Flip";
-// import { ScrollTrigger } from "gsap/ScrollTrigger";
+// document.addEventListener("DOMContentLoaded", (event) => {
 
-console.log('yay2')
+    gsap.registerPlugin(Flip)
 
-document.addEventListener("DOMContentLoaded", (event) => {
-    gsap.registerPlugin(Flip, ScrollTrigger)
+    const gallery = document.querySelector(".portfolio_gallery");
+    const totalNumber = gallery.childElementCount
+
     const portfolioAnchors = document.querySelectorAll(".portfolio_anchor");
-    portfolioAnchors.forEach( (anchor) => {
-        const state = Flip.getState(anchor, { props: "backgroundColor,color" }); // Get the current state
+    portfolioAnchors.forEach((anchor) => {
+        let currentNumber = 0;
+        while( (child = child.previousSibling) != null )
+            currentNumber++;
+        anchor.querySelector(".is-count").innerText =  "/" + totalNumber;
         anchor.addEventListener("click", () => {
-            anchor.classList.toggle("is-active");
+            if (anchor.classList.contains("active")) return;
+
+            const child = anchor.querySelector(".portfolio_slide")
+
+            const state = Flip.getState(child, { props: "padding, top, left, bottom, right, gap" });
+            child.classList.toggle("is-active");
+            document.body.classList.toggle("stop-scrolling");
             Flip.from(state, {
                 duration: 1,
                 ease: "power1.inOut",
-                absolute: true,
-                onComplete: myFunc,
             });
-            console.log("flipped")
         })
     })
-});
+
+//});
