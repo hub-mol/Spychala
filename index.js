b616984981105f161d5a528221f872e71df40fe3
hub-mol <113196877+hub-mol@users.noreply.github.com>
2025-02-18 13:27:11 +0100

    test

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log('git test')
+import PhotoSwipeLightbox from 'https://unpkg.com/photoswipe/dist/photoswipe-lightbox.esm.js';
+
+const lightbox = new PhotoSwipeLightbox({
+    gallery: '#my-gallery',
+    children: 'a',
+    pswpModule: () => import('https://unpkg.com/photoswipe'),
+});
+
+lightbox.init();
+
+
