9a3c207c5d35641c845398241d9ebbe6a21a2b36
hub-mol <113196877+hub-mol@users.noreply.github.com>
2025-02-18 17:43:58 +0100

    edit gsap file

---
+++ gsap-animations.js
@@ -0,0 +1,21 @@
+import { gsap } from "gsap";
+import { Flip } from "gsap/Flip";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+document.addEventListener("DOMContentLoaded", (event) => {
+    gsap.registerPlugin(Flip, ScrollTrigger)
+    const portfolioAnchors = document.querySelectorAll(".portfolio_anchor");
+    portfolioAnchors.forEach( (anchor) => {
+        const state = Flip.getState(anchor, { props: "backgroundColor,color" }); // Get the current state
+        anchor.addEventListener("click", () => {
+            anchor.classList.toggle("is-active");
+            Flip.from(state, {
+                duration: 1,
+                ease: "power1.inOut",
+                absolute: true,
+                onComplete: myFunc,
+            });
+            console.log("flipped")
+        })
+    })
+});
--- photoswipe-lightbox.js
+++ photoswipe-lightbox.js
@@ -1,11 +1,9 @@
 import PhotoSwipeLightbox from 'https://unpkg.com/photoswipe/dist/photoswipe-lightbox.esm.js';
 
 const lightbox = new PhotoSwipeLightbox({
-    gallery: '#my-gallery',
+    gallery: '#my-gallery--responsive-images',
     children: 'a',
     pswpModule: () => import('https://unpkg.com/photoswipe'),
 });
 
 lightbox.init();
-
-
